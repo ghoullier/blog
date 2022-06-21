ef30dad329f6d12ff3f227e05d9cf7b97d320a63
Grégory Houllier <gregory.houllier@gmail.com>
2022-06-21 23:31:51 +0200

    chore: update avatar

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import blog from "https://deno.land/x/blog@0.3.3/blog.tsx";
 blog({
   title: "Grégory Houllier",
   author: "Grégory Houllier",
-  avatar: "./ghoullier.jpeg",
+  avatar: "./ghoullier.png",
   avatarClass: "full",
   links: [
     { title: "Email", url: "mailto:gregory.houllier@gmail.com" },
